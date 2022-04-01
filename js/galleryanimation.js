0da30685d22718df7d04a4ab93415c79b9dc8f73
Dex <67872150+DexTheExplorer@users.noreply.github.com>
2022-04-01 16:04:09 +0200

    Add BETA-Navigation
    My attempt at using JS to add elements dynamically to my page and not request a new HTML document each time. Only accessible at /betanavigation.html currently

--- js/galleryanimation.js
+++
@@ -1,9 +0,0 @@
-/*var redtext = "color: red !important;";
-
-// apply style to same name elements
-function applyStyleById(element, style) {
-    var elements = document.getElementById(element);
-    elements.style.cssText = style;
-}
-
-applyStyleById("project", redtext); // this errors out for some reason*/
